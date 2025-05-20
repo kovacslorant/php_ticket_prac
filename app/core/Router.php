d7e5199e056eaec302993da0b5c3c5751944dc03
Kovacs Lorant <kovacslorant03@gmail.com>
2025-05-20 15:10:25 +0200

    egyszerű routing

---
+++ app/core/Router.php
@@ -0,0 +1,33 @@
+<?php
+
+class Router
+{
+    public function route()
+    {
+        $url = $_GET['url'] ?? 'ticket/index';
+        $parts = explode('/', trim($url, '/'));
+
+        $controllerName = ucfirst($parts[0]) . 'Controller';
+        $method = $parts[1] ?? 'index'; 
+
+        $controllerFile = '../app/controllers/' . $controllerName . '.php';
+
+        if(file_exists($controllerFile)){
+            require_once $controllerFile;
+            $controller = new $controllerName;
+            if(method_exists($controller, $method)){
+                $controller->$method();
+            } else {
+                echo "Metódus nem található: $method";
+            }
+    
+        } else {
+            echo "Kontroller nem található: $ControllerName";
+        }
+    }
+
+}
+
+
+
+?>
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../app/core/Router.php';
+require_once '../app/core/Crontroller.php';
+
+$router = new Router();
+$router->route();
+
+
+?>
