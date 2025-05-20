b7ff4ac2682746c241ece88c39fc3dc672cb5ef4
Kovacs Lorant <kovacslorant03@gmail.com>
2025-05-20 16:07:01 +0200

    TicketController.php typo fix

--- app/controllers/TicketController.php
+++ app/controllers/TicketController.php
@@ -6,7 +6,7 @@ class TicketController extends Controller
     public function index()
     {
         $ticketModel = $this->model('Ticket');
-        $tickets / $ticketModel->getAll();
+        $tickets = $ticketModel->getAll();
         $this->view('tickets/index' , ['tickets' => $tickets]);
     }
 }
